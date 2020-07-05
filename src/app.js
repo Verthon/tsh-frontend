9afa0da88b0310e60d238febee515d91a8f7783d
Verthon <krzysiekksordyl@gmail.com>
2020-07-05 10:26:55 +0200

    feat: add simple validation for username input

--- src/app.js
+++ src/app.js
@@ -1,32 +1,37 @@
-import './assets/scss/app.scss';
-import $ from 'cash-dom';
-
+import $ from "cash-dom";
+import { validateUserName } from "./validate";
+import { doFetch } from "./fetch";
+import "./assets/scss/app.scss";
 
 export class App {
   constructor() {
-    this.initializeApp = this.initializeApp.bind(this)
+    this.initializeApp = this.initializeApp.bind(this);
   }
   initializeApp() {
     let self = this;
 
-    $('.load-username').on('click', function (e) {
-      let userName = $('.username.input').val();
-
-      fetch('https://api.github.com/users/' + userName)
-        .then((response)=> response.json())
-        .then((body) => {
+    $(".load-username").on("click", function (e) {
+      let userName = $("#username").val();
+      if (validateUserName(userName)) {
+        return doFetch(userName).then((body) => {
+          $("#username").removeClass("is-danger");
           self.profile = body;
-          self.update_profile();
+          return self.update_profile();
+        }).catch((error) => {
+          console.log('network error', error)
+          $("#username").addClass("is-danger");
         })
-
-    })
-
+      }
+      $("#username").addClass("is-danger");
+    });
   }
 
   update_profile() {
-    $('#profile-name').text($('.username.input').val())
-    $('#profile-image').attr('src', this.profile.avatar_url)
-    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
-    $('#profile-bio').text(this.profile.bio || '(no information)')
+    $("#profile-name").text($("#username").val());
+    $("#profile-image").attr("src", this.profile.avatar_url);
+    $("#profile-url")
+      .attr("href", this.profile.html_url)
+      .text(this.profile.login);
+    $("#profile-bio").text(this.profile.bio || "(no information)");
   }
 }
